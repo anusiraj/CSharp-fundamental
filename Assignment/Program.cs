66a69aeb7c73e00ca54b6d48cd264fee772ec330
Anu Siraj <anusiraj007@gmail.com>
2023-01-19 14:13:10 +0100

    Partial o/p of Challenge1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -3,37 +3,37 @@
     static void Main(string[] args)
     {
         //Challenge 1
-        int[][] arr1 = { new int[] { 1, 2 }, new int[] { 2, 1, 5 } };
+        int[][] arr1 = { new int[] { 1, 2}, new int[] { 2, 1, 5 } };
         int[] arr1Common = CommonItems(arr1);
         /* write method to print arr1Common */
 
         //Challenge 2
         int[][] arr2 = { new int[] { 1, 2 }, new int[] { 1, 2, 3 } };
-        InverseJagged(arr2);
+        // InverseJagged(arr2);
         /* write method to print arr2 */
 
         //Challenge 3
         int[][] arr3 = { new int[] { 1, 2 }, new int[] { 1, 2, 3 } };
-        CalculateDiff(arr3);
+        // CalculateDiff(arr3);
         /* write method to print arr3 */
 
         //Challenge 4
         int[,] arr4 = { { 1, 2, 3 }, { 4, 5, 6 } };
-        int[,] arr4Inverse = InverseRec(arr4);
+        // int[,] arr4Inverse = InverseRec(arr4);
         /* write method to print arr4Inverse */
 
         //Challenge 5
-        Demo("hello", 1, 2, "world");
+        // Demo("hello", 1, 2, "world");
 
         //Challenge 6
 
         //Challenge 7
-        string firstName, middleName, lastName;
-        ParseNames("Mary Elizabeth Smith", firstName, middleName, lastName);
-        Console.WriteLine($"First name: {firstName}, middle name: {middleName}, last name: {lastName}");
+        // string firstName, middleName, lastName;
+        // ParseNames("Mary Elizabeth Smith", firstName, middleName, lastName);
+        // Console.WriteLine($"First name: {firstName}, middle name: {middleName}, last name: {lastName}");
 
         //Challenge 8
-        GuessingGame();
+        // GuessingGame();
     }
 
     /*
@@ -41,41 +41,57 @@
     Find the common elements in the nested arrays.
     Example: int[][] arr = { new int[] {1, 2}, new int[] {2, 1, 5}}
     Expected result: int[] {1,2} since 1 and 2 are both available in sub arrays.
-    */
+    // */
     static int[] CommonItems(int[][] jaggedArray)
     {
-
+        int[] result = new int[2];
+        int m = 0;
+        for(int i =0; i < jaggedArray.Length; i++){
+            for( int j = 1; j< jaggedArray[i].Length; j++){
+                for(int k = 0; k< jaggedArray[j].Length; k++ ){
+                     if(jaggedArray[m][i] == jaggedArray[j][k]) {
+                    // Console.WriteLine(jaggedArray[j][k]);
+                    result[i] = jaggedArray[j][k];
+                    Console.Write(result[i]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+         }
+         return result;
+       
     }
+
 
     /* 
     Challenge 2. Inverse the elements of a jagged array.
     For example, int[][] arr = {new int[] {1,2}, new int[]{1,2,3}} 
     Expected result: int[][] arr = {new int[]{2, 1}, new int[]{3, 2, 1}}
      */
-    static void InverseJagged(int[][] jaggedArray)
-    {
+    // static void InverseJagged(int[][] jaggedArray)
+    // {
 
-    }
+    // }
 
     /* 
     Challenge 3.Find the difference between 2 consecutive elements of an array.
     For example, int[][] arr = {new int[] {1,2}, new int[]{1,2,3}} 
     Expected result: int[][] arr = {new int[] {-1}, new int[]{-1, -1}}
      */
-    static void CalculateDiff(int[][] jaggedArray)
-    {
+    // static void CalculateDiff(int[][] jaggedArray)
+    // {
 
-    }
+    // }
 
     /* 
     Challenge 4. Inverse column/row of a rectangular array.
     For example, given: int[,] arr = {{1,2,3}, {4,5,6}}
     Expected result: {{1,2},{3,4},{5,6}}
      */
-    static int[,] InverseRec(int[,] recArray)
-    {
+    // static int[,] InverseRec(int[,] recArray)
+    // {
 
-    }
+    // }
 
     /* 
     Challenge 5. Write a function that accepts a variable number of params of any of these types: 
@@ -85,36 +101,36 @@
     - Finally print everything out. 
     Example: Demo("hello", 1, 2, "world") 
     Expected result: hello world; 3 */
-    static void Demo()
-    {
+    // static void Demo()
+    // {
 
-    }
+    // }
 
     /* Challenge 6. Write a function to swap 2 objects but only if they are of the same type 
     and if they’re string, lengths have to be more than 5. 
     If they’re numbers, they have to be more than 18. */
-    static void SwapTwo()
-    {
+    // static void SwapTwo()
+    // {
 
-    }
+    // }
 
     /* Challenge 7. Write a function to parse the first name, middle name, last name given a string. 
     The names will be returned by using out modifier */
-    static void ParseNames(
-        string input,
-        out string firstName,
-        out string middleName,
-        out string lastName)
-    {
+    // static void ParseNames(
+    //     string input,
+    //     out string firstName,
+    //     out string middleName,
+    //     // out string lastName)
+    // {
 
-    }
+    // }
 
     /* Challenge 8. Write a function that does the guessing game. 
     The function will think of a random integer number (lets say within 100) 
     and ask the user to input a guess. 
     It’ll repeat the asking until the user puts the correct answer. */
-    static void GuessingGame()
-    {
+//     static void GuessingGame()
+//     {
 
-    }
+//     }
 }
